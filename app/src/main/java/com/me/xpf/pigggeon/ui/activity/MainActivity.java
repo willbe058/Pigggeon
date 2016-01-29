b36c1474c6c54120253a649d36de888e9bc4c2ab
willbe058 <willbe058@gmail.com>
2016-01-29 15:46:45 +0800

    a little

--- app/src/main/java/com/me/xpf/pigggeon/ui/activity/MainActivity.java
+++ app/src/main/java/com/me/xpf/pigggeon/ui/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.me.xpf.pigggeon;
+package com.me.xpf.pigggeon.ui.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.me.xpf.pigggeon.R;
 
 public class MainActivity extends AppCompatActivity {
 
